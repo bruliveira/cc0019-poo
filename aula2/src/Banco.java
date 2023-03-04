470ae29af309f5163012825b8d636f8aa669ccde
klugaut.bruna.oliveira <bruna.oliveira@klugaut.com>
2023-03-04 01:06:24 -0300

    Aula 2, com atividade

---
+++ aula2/src/Banco.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Banco {
+    public static void main(String[] args) {
+
+        int op;
+        float saldo = 0, valorSaque, valorDeposito;
+        
+        Scanner scan = new Scanner(System.in);
+        do{
+            System.out.println("\n----- Menu principal -----\n1 - Consultar saldo\n2 - Saque\n3 - Depósito\n4 - Sair");
+            System.out.print("Por favor, digite a opçaõ desejada: ");
+            op = scan.nextInt();
+            switch(op){
+                case 1:
+                    System.out.println("\n-> Consultar Saldo");
+                    System.out.println("Saldo disponível: " + saldo);
+                    break;
+                case 2:
+                    System.out.println("\n-> Saque");
+                    System.out.println("Valor desejado para saque: ");
+                    valorSaque = scan.nextFloat();
+                    if(valorSaque > saldo){
+                        System.out.println("Saldo insuficiente!\nSaldo Atual: " + saldo);
+                    }else{
+                        saldo -= valorSaque;
+                        System.out.println("Saque realizado com sucesso!\nSaldo Atual: " + saldo);
+                    }
+                    break;
+                case 3:
+                    System.out.println("\n-> Depósito");
+                    System.out.print("Valor desejado para o depósito: ");
+                    valorDeposito = scan.nextFloat();
+                    if(valorDeposito < 1){
+                        System.out.println("Por favor, digite um valor positvo");
+                    }else{
+                        saldo += valorDeposito;
+                        System.out.println("Depósito realizado com sucesso!\nSaldo Atual: " + saldo);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Poxa, até a próxima!");
+                    break;
+                default:
+                    System.out.println("Opção não encontrada, tente novamente.");
+                    break;
+            }
+        }while(op != 4);
+    
+    }
+}
